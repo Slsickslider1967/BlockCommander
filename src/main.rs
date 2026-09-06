88c0e91a9aea8c5d52344b96711cbd42dad7c40e
Slsickslider1967 <georgiorcocco@outlook.com>
2026-09-06 18:28:24 +0100

    Commands: Added inital commands however as of right now they have no function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+ use clap::{Parser, Subcommand};
+
+ #[derive(Parser)]
+ #[command(name = "BlockCommander")]
+ #[command(about = "Manage Minecraft servers from the terminal")]
+ struct CLI 
+ {
+    #[command(subcommand)]
+    command: Commands,
+ }
+
+ #[derive(Subcommand)]
+ enum Commands
+ {
+    Create { name: String, version: String },
+    List,
+    Setwfolder { dir: String },
+    Run { target: String },
+ }
+
+fn main()
+{
+    let cli = CLI::parse();
+
+    match cli.command
+    {
+        Commands::Create { name, version } => {
+            println!("Creating server '{}' with version '{}'", name, version);
+        }
+        Commands::List => {
+            println!("Listing all servers...");
+        }
+        Commands::Setwfolder { dir } => {
+            println!("Setting working folder to '{}'", dir);
+        }
+        Commands::Run { target } => {
+            println!("Running server '{}'", target);
+        }
+    }
 }
